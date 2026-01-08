43d7356363f3afcd80f4a4cd80963484815d6fe5
Aayush Chalise <chaliseaayush2006@gmail.com>
2026-01-09 00:24:17 +0545

    add constant file to constants folder with a constant navLinks and also add Navbar component to components folder

---
+++ frontend/myportfolio/src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <header className="w-full bg-yellow-200">
+      <nav></nav>
+    </header>
+  );
+};
+
+export default Nav;
---
+++ frontend/myportfolio/src/constants/constant.js
@@ -0,0 +1,14 @@
+const navLinks = [
+  {
+    href: "#home",
+    label: "Home",
+  },
+  {
+    href: "#contacts",
+    label: "Contacts",
+  },
+  {
+    href: "#resume",
+    label: "Resume",
+  },
+];
