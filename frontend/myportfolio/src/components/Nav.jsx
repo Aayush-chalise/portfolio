caf1f078e1d8be7c0130c447bde7c3526f368ddb
Aayush Chalise <chaliseaayush2006@gmail.com>
2026-01-09 23:04:12 +0545

    add basic styling to global css file

--- frontend/myportfolio/src/components/Nav.jsx
+++ frontend/myportfolio/src/components/Nav.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <header className="w-full border-2 fixed border-black z-10  px-4 py-7  ">
-      <nav className="flex flex-wrap justify-between items-center  border-2 border-blue-500 max-w-3xl  xl:max-w-5xl lg:max-w-4xl mx-auto">
+    <header className="w-full  fixed  z-10  px-4 py-7  border border-black/10">
+      <nav className="flex flex-wrap justify-between items-center     max-w-3xl  xl:max-w-5xl lg:max-w-4xl mx-auto">
         <div>aayush</div>
         <div className="hidden md:flex">
-          <ul className="flex border-2 border-red-500 gap-8 ">
+          <ul className="flex    gap-8 ">
             {navLinks.map((item) => (
               <li key={item.label}>
                 <Link>{item.label}</Link>
