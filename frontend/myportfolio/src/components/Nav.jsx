2c0ae11db8fadd466121f339185bdbfa1954b22e
Aayush Chalise <chaliseaayush2006@gmail.com>
2026-01-11 12:52:00 +0545

    add hamburger

--- frontend/myportfolio/src/components/Nav.jsx
+++ frontend/myportfolio/src/components/Nav.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { navLinks } from "../constants/constant";
 import { Link } from "react-router-dom";
+import { Menu } from "lucide-react";
+import { useState } from "react";
 
 const Nav = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  console.log(isMenuOpen);
   return (
-    <header className="w-full  fixed  z-10  px-4 py-7  border border-black/10">
+    <header className="w-full  fixed  z-10  px-4 py-7  border border-black/10 bg-bg-color font-medium">
       <nav className="flex flex-wrap justify-between items-center     max-w-3xl  xl:max-w-5xl lg:max-w-4xl mx-auto  font-dm-sans">
         <div>aayush</div>
         <div className="hidden md:flex">
@@ -15,6 +19,11 @@ const Nav = () => {
               </li>
             ))}
           </ul>
+        </div>
+        <div className="md:hidden">
+          <button onClick={() => setIsMenuOpen(true)}>
+            <Menu></Menu>
+          </button>
         </div>
       </nav>
     </header>
