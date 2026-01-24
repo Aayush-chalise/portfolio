922e6e1eeed321109c8198278a938fd011a5843c
Aayush Chalise <chaliseaayush2006@gmail.com>
2026-01-24 09:15:58 +0545

    add new projects

--- frontend/myportfolio/src/constants/constant.js
+++ frontend/myportfolio/src/constants/constant.js
@@ -9,18 +9,18 @@ export const projects = [
     image: newsmania,
   },
   {
-    name: "NewsMania",
+    name: "HomeLabCare",
     description:
-      "A web application that provides the latest news articles from various sources, allowing users to stay informed on current events.",
-    githubLink: "https://github.com/Aayush-chalise/NewsMania",
-    image: newsmania,
+      "HomeLabCare is a platform where user can book their lab services online and get a quote for the service they need from our team of highly qualified professionals. Also they can view their reports online and show it to their doctor.",
+    githubLink: "https://github.com/Aayush-chalise/HomeLabCare",
+    image: healthlabcare,
   },
   {
-    name: "NewsMania",
+    name: "Nike Clone",
     description:
-      "A web application that provides the latest news articles from various sources, allowing users to stay informed on current events.",
-    githubLink: "https://github.com/Aayush-chalise/NewsMania",
-    image: newsmania,
+      "A responsive clone of the Nike website built with React and Tailwind CSS, featuring a modern design and interactive elements.",
+    githubLink: "https://github.com/Aayush-chalise/nikeweb",
+    image: nike,
   },
   {
     name: "NewsMania",
