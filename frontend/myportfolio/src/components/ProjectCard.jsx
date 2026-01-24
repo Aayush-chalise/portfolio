0c545e5c1b2e8138bbca97e5dcb134013fde7a36
Aayush Chalise <chaliseaayush2006@gmail.com>
2026-01-24 15:44:14 +0545

    add Footer

--- frontend/myportfolio/src/components/ProjectCard.jsx
+++ frontend/myportfolio/src/components/ProjectCard.jsx
@@ -3,14 +3,18 @@ import { FaGithub } from "react-icons/fa";
 
 const ProjectCard = ({ project }) => {
   return (
-    <div className=" flex max-sm:flex-col  bg-card-bg   font-dm-sans mt-6 rounded-2xl w-full text-theme-green p-4  max-w-5xl  ">
-      <div className="p-3   gap-4   md:w-[55%]   ">
-        <h4 className="text-3xl  md:text-4xl font-dm-serif ">{project.name}</h4>
-        <p className=" leading-loose text-base p-1 mt-2 text-description ">
-          {project.description}
-        </p>
-        <p>Built-with:</p>
-        <div className="flex gap-4">
+    <div className=" flex max-sm:flex-col  bg-card-bg   font-dm-sans mt-6 rounded-2xl w-full  max-w-5xl p-1 md:p-3 ">
+      <div className="p-3     md:w-[55%]  flex flex-col justify-center    ">
+        <div>
+          <h4 className="text-3xl  md:text-4xl font-dm-serif  text-theme-green">
+            {project.name}
+          </h4>
+          <p className=" leading-loose text-base p-1 mt-2 text-description ">
+            {project.description}
+          </p>
+        </div>
+
+        {/* <div className="flex gap-4">
           {project.tags.map((tag) => {
             const IconTag = tag.icon;
             return (
@@ -24,16 +28,20 @@ const ProjectCard = ({ project }) => {
               </p>
             );
           })}
-        </div>
-        <div className="  p-1 mt-2 inline-block  text-black px-2">
+        </div> */}
+        <div className="  p-1 mt-2 inline-block  text-black px-2   ">
           <a href={project.githubLink} alt={project.name} target="_blank">
             <FaGithub size={24} />
           </a>
         </div>
       </div>
 
-      <div className=" rounded-2xl   md:w-[45%]  ">
-        <img src={project.image} alt={project.name} className="rounded-2xl " />
+      <div className=" rounded-2xl   md:w-[45%]    overflow-hidden   ">
+        <img
+          src={project.image}
+          alt={project.name}
+          className="rounded-2xl w-full h-auto   "
+        />
       </div>
     </div>
   );
